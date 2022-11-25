6dca136f279c74bd637bafc3dcb992fbfec6ed14
baptiste-cornic <baptiste.cornic92@gmail.com>
2022-11-25 16:02:54 +0100

    Fix : PurchaseService

--- src/Service/PurchaseService.php
+++ src/Service/PurchaseService.php
@@ -20,15 +20,16 @@ class PurchaseService
         $sessionCart = $this->requestStack->getSession()->get('cart');
         $cart = [];
         $totalPrice = 0;
-
-        foreach ($sessionCart as $id => $quantity){
-            $product = $this->productRepo->find($id);
-            if($product){
-                $cart[$id]=[
-                    'quantity' => $quantity,
-                    'product' => $product
-                ];
-                $totalPrice = $totalPrice + $product->getPrice() * $quantity;
+        if ($sessionCart){
+            foreach ($sessionCart as $id => $quantity){
+                $product = $this->productRepo->find($id);
+                if($product){
+                    $cart[$id]=[
+                        'quantity' => $quantity,
+                        'product' => $product
+                    ];
+                    $totalPrice = $totalPrice + $product->getPrice() * $quantity;
+                }
             }
         }
 
